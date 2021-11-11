a1b56843d39597b6eaf4b7daa1a5f75b25f8d1b8
tjxy <tjxy@tjxys-Mac-mini.local>
2021-11-12 02:01:40 +0800

    lint, perspective init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from "react";
 import {
-  Router,
-  BrowserRouter,
-  Route,
-  Routes,
-  Link,
-  Redirect,
-  useHistory,
-  useLocation,
+    BrowserRouter,
+    Link,
+    Redirect,
+    Route,
+    Router,
+    Routes,
+    useHistory,
+    useLocation
 } from "react-router-dom";
 import "./App.css";
 import SiteList from "./Pages/SiteList/SiteList";
 
 
 const App = () => {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<SiteList  />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={ <SiteList  /> } />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
--- src/Pages/SiteList/SiteList.jsx
+++ src/Pages/SiteList/SiteList.jsx
@@ -1,25 +1,104 @@
 import styled, { css } from "styled-components";
+import React from "react";
 import useSWR from "swr";
 // components
 const Site = ({ data }) => {
   //   const { imageCover } = data;
+  //   console.log("data", data);
   return (
-    <div className="flex space-x-4" style={{ backgroundColor: "yellow" }}>
-      <div className="flex-1">1</div>
+    <div
+      style={{
+        flex: "0 0 100%",
+        width: "400px",
+        height: "300px",
+        // backgroundColor: "red",
+      }}
+    >
+      {/* <div style={{ height: `300px` }}> */}
+      <img
+        // className="flex"
+        src={`${process.env.REACT_APP_API_ROOT_URL}/images/sites/${data.imageCover}`}
+        alt={data?.slug}
+      />
     </div>
   );
 };
 
-const List = () => {
+const List = ({ sitesData }) => {
   return (
-    <div className="flex space-x-4" style={{ backgroundColor: "blue" }}>
-      <Site />
-      <Site />
-      <Site />
+    <div
+      className="relative"
+      style={{
+        top: "20%",
+      }}
+    >
+      <div
+        className="w-screen flex justify-center"
+        style={{
+          backgroundColor: "yellow",
+          // width: "400px",
+          // height: "300px",
+          // transform: "rotateY(-10deg) translateX(1.5rem)",
+          // transformStyle: "preserve-3d",
+          // overflowX: "scroll",
+          //   perspective: "800px",
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: "blue",
+            width: "400px",
+            height: "300px",
+            // maxWidth: "80%",
+            // maxHeight: "30%",
+          }}
+        >
+          <div className="absolute" style={{ width: "100%" }}>
+            <div
+              className="fixed"
+              style={{
+                width: "100%",
+                perspective: "800px",
+              }}
+            >
+              <div
+                className="flex"
+                style={{
+                  overflowX: "auto",
+                  width: "100%",
+                  transformOrigin: "0 0",
+                  transform: "rotateY(-15deg)",
+                }}
+              >
+                {/* <div
+                  style={{
+                    flex: "0 0 100%",
+                    width: "4000px",
+                    height: "100px",
+                    backgroundColor: "red",
+                  }}
+                ></div>
+                <div
+                  style={{
+                    flex: "0 0 100%",
+                    width: "4000px",
+                    height: "100px",
+                    backgroundColor: "purple",
+                  }}
+                ></div> */}
+
+                {sitesData &&
+                  (sitesData ?? []).map((site) => (
+                    <Site data={site} key={site?.slug} />
+                  ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
-
 // page
 const SiteList = () => {
   const { data, error, mutate } = useSWR(
@@ -30,14 +109,94 @@ const SiteList = () => {
         .then((res) => res?.data?.data)
   );
   return (
-    <div className="h-screen" style={{ backgroundColor: "#FFB7C5" }}>
-      <div
-        className="absolute"
-        style={{ backgroundColor: "orange", top: "30%" }}
-      >
-        <List data={data} />
-      </div>
+    <div
+      className="h-screen"
+      style={{
+        //    perspective: "800px",
+        backgroundColor: "orange",
+      }}
+    >
+      <List sitesData={data} />
     </div>
+    // <div className="fixed" style={{ height: "6em" }}>
+    //   <div
+    //     className="flex"
+    //     style={{ overflowX: "scroll", height: "6em", width: "100vw" }}
+    //   >
+    //     <div
+    //       style={{
+    //         flex: "0 0 100%",
+    //         height: "6em",
+    //         //   width: "4000px",
+    //         //   height: "100px",
+    //         backgroundColor: "red",
+    //       }}
+    //     ></div>
+    //     <div
+    //       style={{
+    //         flex: "0 0 100%",
+    //         height: "6em",
+    //         //   width: "4000px",
+    //         //   height: "100px",
+    //         backgroundColor: "blue",
+    //       }}
+    //     ></div>
+    //     <div
+    //       style={{
+    //         flex: "0 0 100%",
+    //         height: "6em",
+    //         //   width: "4000px",
+    //         //   height: "100px",
+    //         backgroundColor: "red",
+    //       }}
+    //     ></div>
+    //     <div
+    //       style={{
+    //         flex: "0 0 100%",
+    //         height: "6em",
+    //         //   width: "4000px",
+    //         //   height: "100px",
+    //         backgroundColor: "blue",
+    //       }}
+    //     ></div>
+    //     <div
+    //       style={{
+    //         flex: "0 0 100%",
+    //         height: "6em",
+    //         //   width: "4000px",
+    //         //   height: "100px",
+    //         backgroundColor: "red",
+    //       }}
+    //     ></div>
+    //     <div
+    //       style={{
+    //         flex: "0 0 100%",
+    //         height: "6em",
+    //         //   width: "4000px",
+    //         //   height: "100px",
+    //         backgroundColor: "blue",
+    //       }}
+    //     ></div>
+    //     <div
+    //       style={{
+    //         flex: "0 0 100%",
+    //         height: "6em",
+    //         //   width: "4000px",
+    //         //   height: "100px",
+    //         backgroundColor: "red",
+    //       }}
+    //     ></div>
+    //     <div
+    //       style={{
+    //         flex: "0 0 100%",
+    //         height: "6em",
+    //         //   width: "4000px",
+    //         //   height: "100px",
+    //         backgroundColor: "blue",
+    //       }}
+    //     ></div>
+    //   </div>
+    // </div>
   );
 };
 export default SiteList;
