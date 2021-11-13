b16141e94b75cc86cfd225b13f319ba49a5e1e63
tjxy <tjxy@tjxys-Mac-mini.local>
2021-11-14 02:03:52 +0800

    list init

--- src/Pages/SiteList/SiteList.jsx
+++ src/Pages/SiteList/SiteList.jsx
@@ -16,103 +16,31 @@ import { ScrollToPlugin } from "gsap/ScrollToPlugin";
 import { useDrag } from "@use-gesture/react";
 // import { debounce } from "../../utils";
 
-gsap.registerPlugin(ScrollToPlugin);
+// gsap.registerPlugin(ScrollToPlugin);
 
-// gsap.registerPlugin(ScrollTrigger);
-
-// ScrollTrigger.defaults({
-//   toggleActions: "restart pause resume pause",
-//   scroller: "#container",
-// });
-
-const Site = ({ data, siteIndex }) => {
+const SiteCard = ({ siteData }) => {
+  const { name, description, imageCover } = siteData;
   return (
     <div
-      id={`site_${siteIndex}`}
-      className="panel flex"
-      style={{
-        flex: "0 0 100%",
-        scrollSnapAlign: "start",
-        width: "100vw",
-        justifyContent: "center",
-        border: "1px solid red",
-        height: "300px",
-      }}
+      className="w-full bg-white sahdow-lg overflow-hidden flex flex-col justify-center items-center"
+      style={{ border: "6px solid black" }}
     >
-      {/* <img
-        src={`${process.env.REACT_APP_API_ROOT_URL}/images/sites/${data.imageCover}`}
-        alt={data?.slug}
-        style={{ width: "100%" }}
-      /> */}
-    </div>
-  );
-};
-
-const List = ({ sitesData }) => {
-  return (
-    <div
-      className="relative"
-      style={{
-        top: "20%",
-      }}
-    >
-      <div
-        className="w-screen flex justify-center"
-        style={
-          {
-            // backgroundColor: "yellow",
-            // width: "400px",
-            // height: "300px",
-            // transform: "rotateY(-10deg) translateX(1.5rem)",
-            // transformStyle: "preserve-3d",
-            // overflowX: "scroll",
-            //   perspective: "800px",
-          }
-        }
-      >
-        <div
-          style={{
-            backgroundColor: "blue",
-            width: "100%",
-            height: "300px",
-            // maxWidth: "50%",
-            // maxHeight: "30%",
-          }}
-        >
-          <div className="absolute" style={{ width: "100%" }}>
-            <div
-              className="fixed"
-              style={{
-                width: "100%",
-                // perspective: "800px",
-              }}
-            >
-              {/* <div
-                id="container"
-                className="container"
-                style={{
-                  maxHeight: "100vh",
-                  maxWidth: "100vw",
-                  overflowX: "scroll",
-                  display: "flex",
-                  // scrollSnapType: "x mandatory",
-                  transformOrigin: "0 0",
-                  // border: "2px solid blue",
-                  // transform: "rotateY(-15deg)",
-                }}
-              >
-                {sitesData &&
-                  (sitesData ?? []).map((site, index) => (
-                    <Site data={site} key={site?.slug} siteIndex={index} />
-                  ))}
-              </div> */}
-            </div>
-          </div>
-        </div>
+      <div>
+        <img
+          className="object-center object-cover h-auto w-full"
+          src={`${process.env.REACT_APP_API_ROOT_URL}/images/sites/${imageCover}`}
+          // style={{ border: "1px solid red" }}
+          alt="photos"
+        />
+      </div>
+      <div className="text-center py-8 sm:py-6" style={{ minHeight: "160px" }}>
+        <p className="text-xl text-gray-700 font-bold mb-2">{name.en}</p>
+        <p className="text-sm text-gray-800 font-light">{description}</p>
       </div>
     </div>
   );
 };
+
 // page
 const SiteList = () => {
   const { data, error, mutate } = useSWR(
@@ -122,74 +50,30 @@ const SiteList = () => {
         .then((res) => res.json())
         .then((res) => res?.data?.data)
   );
-  const bind = useDrag((dragData) => {
-    const { movement, last } = dragData;
-    if (last) {
-      onDrag(movement);
-    }
-  });
-
-  const scrollToSite = (siteId) => {
-    gsap.to("#container", {
-      duration: 1.3,
-      scrollTo: `#site_${siteId}`,
-      ease: "power2",
-    });
-  };
-
-  const container = document.querySelector("#container");
-
-  const onDrag = useCallback(
-    (movement) => {
-      console.log("onDrag");
-      // if (my < -50) {
-      //   console.log("dragged up");
-      // }
-
-      if (container) {
-        const observer = new IntersectionObserver(
-          (entries) => {
-            const currentSite = entries.find(
-              (entry) => entry.intersectionRatio > 0.7
-            );
-            if (currentSite) {
-              const currentSiteIndex = Number(currentSite.target.id.slice(5));
-              const mx = movement[0];
-              if (mx > 35) {
-                console.log("dragged to right");
-                scrollToSite(currentSiteIndex - 1);
-              }
-              if (mx < -35) {
-                console.log("dragged to left");
-                scrollToSite(currentSiteIndex + 1);
-              }
-            }
-          },
-          { threshold: [0], root: container }
-        );
-
-        const sitesObserve = data.map((site, index) => {
-          observer.observe(
-            document.querySelector(`#container > #site_${index}`)
-          );
-        });
-      }
-    },
-    [container]
-  );
 
   return (
-    <div
+    <section
       draggable={false}
-      className="h-screen"
-      {...bind()}
+      className=""
       style={{
+        // border: "2px solid red",
         touchAction: "none",
-        // backgroundColor: "orange",
       }}
     >
-      <List sitesData={data} />
-    </div>
+      {/* <div className="text-center pb-12"> */}
+      {/* <h2 className="text-base font-bold text-indigo-600">
+          We have the best equipment in the market
+        </h2>
+        <h1 className="font-bold text-3xl md:text-4xl lg:text-5xl font-heading text-gray-900">
+          Check our awesome team members
+        </h1> */}
+      {/* </div> */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+        {(data ?? []).map((siteData, index) => (
+          <SiteCard key={index} siteData={siteData} />
+        ))}
+      </div>
+    </section>
   );
 };
 export default SiteList;
