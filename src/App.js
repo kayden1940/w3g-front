8128002506eaa0f7ec422bae3165586aedc744b1
tjxy <tjxy@tjxys-Mac-mini.local>
2021-11-13 19:24:00 +0800

    siteList wip

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import SiteList from "./Pages/SiteList/SiteList";
 const App = () => {
     return (
         <div className="App">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no,maximum-scale=1"/>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={ <SiteList  /> } />
--- src/Pages/SiteList/SiteList.jsx
+++ src/Pages/SiteList/SiteList.jsx
@@ -1,22 +1,34 @@
 /* eslint-disable react/display-name */
 import styled, { css } from "styled-components";
-import React, { forwardRef, useRef, useState, useEffect } from "react";
+import React, {
+  forwardRef,
+  useRef,
+  useState,
+  useEffect,
+  useCallback,
+} from "react";
 import useSWR from "swr";
 import { motion, useTransform, useElementScroll } from "framer-motion";
-import { useWindowSize } from "../../hooks/useWindowSize";
+// import { useWindowSize } from "../../hooks/useWindowSize";
 import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { useDrag } from "@use-gesture/react";
+// import { debounce } from "../../utils";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin);
 
-ScrollTrigger.defaults({
-  toggleActions: "restart pause resume pause",
-  scroller: ".container",
-});
+// gsap.registerPlugin(ScrollTrigger);
 
-const Site = ({ data }) => {
+// ScrollTrigger.defaults({
+//   toggleActions: "restart pause resume pause",
+//   scroller: "#container",
+// });
+
+const Site = ({ data, siteIndex }) => {
   return (
     <div
+      id={`site_${siteIndex}`}
       className="panel flex"
       style={{
         flex: "0 0 100%",
@@ -27,11 +39,11 @@ const Site = ({ data }) => {
         height: "300px",
       }}
     >
-      <img
+      {/* <img
         src={`${process.env.REACT_APP_API_ROOT_URL}/images/sites/${data.imageCover}`}
         alt={data?.slug}
-        style={{ width: "90%" }}
-      />
+        style={{ width: "100%" }}
+      /> */}
     </div>
   );
 };
@@ -60,7 +72,7 @@ const List = ({ sitesData }) => {
       >
         <div
           style={{
-            // backgroundColor: "blue",
+            backgroundColor: "blue",
             width: "100%",
             height: "300px",
             // maxWidth: "50%",
@@ -72,28 +84,28 @@ const List = ({ sitesData }) => {
               className="fixed"
               style={{
                 width: "100%",
-                perspective: "800px",
+                // perspective: "800px",
               }}
             >
-              <div
+              {/* <div
+                id="container"
                 className="container"
                 style={{
                   maxHeight: "100vh",
                   maxWidth: "100vw",
                   overflowX: "scroll",
                   display: "flex",
-                  scrollSnapType: "x mandatory",
+                  // scrollSnapType: "x mandatory",
                   transformOrigin: "0 0",
-                  border: "2px solid blue",
-                  transform: "rotateY(-15deg)",
+                  // border: "2px solid blue",
+                  // transform: "rotateY(-15deg)",
                 }}
               >
-            
                 {sitesData &&
-                  (sitesData ?? []).map((site) => (
-                    <Site data={site} key={site?.slug} />
+                  (sitesData ?? []).map((site, index) => (
+                    <Site data={site} key={site?.slug} siteIndex={index} />
                   ))}
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
@@ -110,44 +122,73 @@ const SiteList = () => {
         .then((res) => res.json())
         .then((res) => res?.data?.data)
   );
+  const bind = useDrag((dragData) => {
+    const { movement, last } = dragData;
+    if (last) {
+      onDrag(movement);
+    }
+  });
+
+  const scrollToSite = (siteId) => {
+    gsap.to("#container", {
+      duration: 1.3,
+      scrollTo: `#site_${siteId}`,
+      ease: "power2",
+    });
+  };
+
+  const container = document.querySelector("#container");
+
+  const onDrag = useCallback(
+    (movement) => {
+      console.log("onDrag");
+      // if (my < -50) {
+      //   console.log("dragged up");
+      // }
+
+      if (container) {
+        const observer = new IntersectionObserver(
+          (entries) => {
+            const currentSite = entries.find(
+              (entry) => entry.intersectionRatio > 0.7
+            );
+            if (currentSite) {
+              const currentSiteIndex = Number(currentSite.target.id.slice(5));
+              const mx = movement[0];
+              if (mx > 35) {
+                console.log("dragged to right");
+                scrollToSite(currentSiteIndex - 1);
+              }
+              if (mx < -35) {
+                console.log("dragged to left");
+                scrollToSite(currentSiteIndex + 1);
+              }
+            }
+          },
+          { threshold: [0], root: container }
+        );
+
+        const sitesObserve = data.map((site, index) => {
+          observer.observe(
+            document.querySelector(`#container > #site_${index}`)
+          );
+        });
+      }
+    },
+    [container]
+  );
+
   return (
     <div
+      draggable={false}
       className="h-screen"
-      style={
-        {
-          //    perspective: "800px",
-          // backgroundColor: "orange",
-        }
-      }
+      {...bind()}
+      style={{
+        touchAction: "none",
+        // backgroundColor: "orange",
+      }}
     >
       <List sitesData={data} />
-      {/* <div
-        className="container"
-        style={{
-          maxHeight: "100vh",
-          maxWidth: "100vw",
-          overflowX: "scroll",
-          display: "flex",
-          scrollSnapType: "x mandatory",
-        }}
-      >
-        {["1", "2", "3", "4"].map((p) => {
-          return (
-            <div
-              style={{
-                flex: "0 0 100%",
-                border: "2px solid red",
-                scrollSnapAlign: "start",
-                height: "100vh",
-                width: "100vw",
-              }}
-              key={p}
-            >
-              <p>{p}</p>
-            </div>
-          );
-        })}
-      </div> */}
     </div>
   );
 };
