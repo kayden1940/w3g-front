ac985cd4cdae71750c5880d1e6d9d877202a0273
K1940 <82050470+kayden1940@users.noreply.github.com>
2022-12-21 19:53:50 -0700

    fetch know

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,40 @@
-// import '#/styles/globals.css'
-import '#/styles/globals.css';
-import Link from "next/link"
-// import { AddressBar } from '#/ui/AddressBar';
-// import { GlobalNav } from '#/ui/GlobalNav';
-// import { VercelLogo } from '#/ui/VercelLogo';
+import '#/styles/globals.css'
+import { GlobalNav } from '#/ui/GlobalNav';
+// // import { AddressBar } from '#/ui/AddressBar';
+// // import { GlobalNav } from '#/ui/GlobalNav';
+// // import { VercelLogo } from '#/ui/VercelLogo';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html>
-      <body>
-        <main>
-          <nav>
-            <Link href="/">
-              Home
-            </Link>
-            <Link href="/site/submit">
-              Submit site
-            </Link>
-          </nav>
-          {children}
-        </main>
+    <html lang="en" className="[color-scheme:dark]">
+      <head />
+      <body className="overflow-y-scroll bg-gray-1100 bg-[url('/grid.svg')]">
+        <GlobalNav />
+
+        <div className="lg:pl-72">
+          <div className="mx-auto max-w-4xl space-y-8 px-2 pt-20 lg:py-8 lg:px-8">
+            <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
+              <div className="rounded-lg bg-black">
+                {/* <AddressBar /> */}
+              </div>
+            </div>
+
+            <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
+              <div className="rounded-lg bg-black p-3.5 lg:p-6">{children}</div>
+            </div>
+
+            {/* <div className="rounded-lg bg-vc-border-gradient p-px shadow-lg shadow-black/20">
+              <div className="rounded-lg bg-black">
+                <Byline />
+              </div>
+            </div> */}
+          </div>
+        </div>
       </body>
     </html>
-  )
+  );
 }
